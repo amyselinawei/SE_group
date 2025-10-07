f58bac96a7e7589dfd5413a0c9d681fa433cbfa9
amyselinawei <amyselinawei@gmail.com>
2025-10-07 11:21:36 +0800

    Div

--- math.py
+++ math.py
@@ -9,6 +9,10 @@ elif choice == '-':
 elif choice == '*':
     print(f"Result: {multiply(num1, num2)}")
 elif choice == '/':
+    def divide(a, b):
+        if b == 0:
+            return "Error: Cannot divide by zero."
+        return a/b
     print(f"Result: {divide(num1, num2)}")
 else:
     print("Invalid input")
