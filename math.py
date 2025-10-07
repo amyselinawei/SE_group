85822e6484d1e7196f3395d747cfbd168ac94699
amyselinawei <amyselinawei@gmail.com>
2025-10-07 10:35:38 +0800

    完成更改

--- math.py
+++ math.py
@@ -3,12 +3,22 @@ num2 = float(input("Enter second number: "))
 choice = input("Enter choice (+,-,*,/): ")
 
 if choice == '+':
+    def add(a, b):
+        return a + b
     print(f"Result: {add(num1, num2)}")
 elif choice == '-':
+    def subtract(a, b):
+        return a-b
     print(f"Result: {subtract(num1, num2)}")
 elif choice == '*':
+    def multiply(a, b):
+        return a*b
     print(f"Result: {multiply(num1, num2)}")
 elif choice == '/':
+    def divide(a, b):
+        if b == 0:
+            return "Error: Cannot divide by zero."
+        return a/b
     print(f"Result: {divide(num1, num2)}")
 else:
     print("Invalid input")
